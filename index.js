60b4a60dc2f2ea9ce7fbf2aa54db491a0f9bfa94
Kauan Carvalho <kauan3210@gmail.com>
2023-02-04 16:57:01 -0300

    Project finished

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 document.getElementById("get-color-scheme").addEventListener('click', function(){
+    const getColor = document.getElementById("color-picker").value.replace('#','')
+    const paletteChoice = document.getElementById("palette-choice").value.toLowerCase()
+    sendColor(getColor , paletteChoice)
 
-    console.log(document.getElementById("color-picker").value)
+    console.log(paletteChoice)
 })
 
 
+function sendColor(color , palette){
 
-fetch("https://www.thecolorapi.com/scheme", {
+    let colorApi = `https://www.thecolorapi.com/scheme?hex=${color}&mode=${palette}&count=5`
+
+    fetch(`${colorApi}`,{
     method:"GET"
 })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        for(let i = 0; i<5; i++ ){
+            document.getElementById(`color-${i}`).style.backgroundColor=data.colors[i].hex.value
+            document.getElementById(`color-code${i}`).innerHTML =`<p id="color-code${i}">${data.colors[i].hex.value}</p>`
+        }   
+    })
+    
+
+}
